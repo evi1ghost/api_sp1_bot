25afd1c1be21b61cc24446d009723aefdbb68806
Дубинчик Андрей <evil_ghost@bk.ru>
2021-05-28 22:59:49 +0300

    Дополнительная обработка исключений

--- homework.py
+++ homework.py
@@ -23,26 +23,31 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
+    homework_name = homework.get('homework_name')
     statuses = {
-        'reviewing': 'Домашка взатя в ревью.',
+        'reviewing': 'Домашка взата в ревью.',
         'rejected': 'К сожалению в работе нашлись ошибки.',
         'approved': (
             'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
         )
     }
-    verdict = statuses[homework['status']]
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    verdict = statuses.get(homework.get('status'))
+    if not verdict or not homework_name:
+        raise Warning('Неверный ответ сервера')
+    return f'Изменился статус работы "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
-    from_date = {'from_date': current_timestamp}
-    homework_statuses = requests.get(
-        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        headers=headers,
-        params=from_date
-    )
+    params = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(
+            'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+            headers=headers,
+            params=params
+        )
+    except requests.RequestException:
+        raise Warning('Ошибка сервера')
     return homework_statuses.json()
 
 
@@ -54,7 +59,7 @@ def send_message(message, bot_client):
 def main():
     logger.debug('Bot started')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = int(time.time()) - 80000
 
     while True:
         try:
@@ -72,8 +77,11 @@ def main():
         except Exception as e:
             error_message = f'Бот столкнулся с ошибкой: {e}'
             logger.error(error_message)
-            send_message(error_message, bot)
-            time.sleep(5)
+            try:
+                send_message(error_message, bot)
+            except telegram.error.TelegramError:
+                pass
+            time.sleep(300)
 
 
 if __name__ == '__main__':
